576faca97c29dfdb61356f5c7da33b9a11066fd5
Mohan Krishna Gundala <gmk.india@outlook.com>
2017-01-29 01:40:18 +0530

    installed devise

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -21,11 +21,16 @@ class NotesController < ApplicationController
   end
 
   def destroy
-
+    @note.destroy
+    redirect_to notes_path
   end
 
   def update
-
+    if @note.update(note_params)
+      redirect_to @note
+    else
+      render 'edit'
+    end
   end
 
   def show
